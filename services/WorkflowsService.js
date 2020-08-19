4606464bca1226fcd682f2fba856fc924ff03f59
Shlomi Koncha <shlomikoncha@gmail.com>
2020-08-19 11:23:37 +0300

    fix(unit-tests): fixed unit test typos and config

--- services/WorkflowsService.js
+++ services/WorkflowsService.js
@@ -47,10 +47,10 @@ exports.workflowsPOST = async function (args, res, next) {
 
 exports.workflowsGET = async function (args, res, next) {
   try {
-    logger.info(`[WorkflowService] workflowsGET - get workflows: ${files}`);
     const files = await DataHandlerFileSystem.getFilesFromRootPath(
       config.fileSystem.workflowsPath
-    );
+      );
+      logger.info(`[WorkflowService] workflowsGET - workflows: ${files}`);
     res.end(JSON.stringify(files));
   } catch (error) {
     logger.error(
